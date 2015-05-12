68be17fd06009181368e2d3f9ebeeb28a6517264
Ryan Gibbs <ryangibbs11@msn.com>
2015-05-12 10:26:11 +0100

    Moved RequestTask into inner class and changed so that spinners are populated in onPostExecute rather than UI thread. I learned a thing.

--- app/src/main/java/com/alacityfoundation/statistick/MainActivityFragment.java
+++ app/src/main/java/com/alacityfoundation/statistick/MainActivityFragment.java
@@ -19,13 +19,20 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment implements OnTaskCompleted {
+public class MainActivityFragment extends Fragment {
     private ArrayList<Force> forces = new ArrayList<>();
     private ArrayList<Crime> crimes = new ArrayList<>();
     private final ArrayList<String> months = new ArrayList<String>() {{
@@ -54,8 +61,18 @@ public class MainActivityFragment extends Fragment implements OnTaskCompleted {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // perform web request to get list of all police forces
-        new RequestTask(this, new Force()).execute("https://data.police.uk/api/forces");
+        new RequestTask(new Force()).execute("https://data.police.uk/api/forces");
 
+        // return inflated view
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    // called on button press
+    public void getCrime(View view) {
+
+    }
+
+    private void populateSpinners() {
         // loop through results and add to spinner
         Spinner forceSpinner = (Spinner) this.getActivity().findViewById(R.id.forceSpinner);
         ArrayAdapter<String> forceAdapter = new ArrayAdapter(this.getActivity().getBaseContext(), android.R.layout.simple_spinner_item, this.forces);
@@ -70,45 +87,106 @@ public class MainActivityFragment extends Fragment implements OnTaskCompleted {
 
         yearSpinner.setAdapter(yearAdapter);
         monthSpinner.setAdapter(monthAdapter);
-
-        // return inflated view
-        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
-    /**
-     * Processes the results from the AsyncTask
-     *
-     * @param obj The class we expect the results to be instances of, used to deserialise them
-     * @param result The JSON encoded data retrieved from the HTTP Request
-     */
-    @Override
-    public void onTaskCompleted(Object obj, String result) {
-        Log.d("RESULTS", result);
-        try {
-            // deserialise the json objects and add to the appropriate instance variable
-            JSONArray jsonResult = new JSONArray(result);
-            for(int i = 0; i < jsonResult.length(); i++) {
-                Object resultObject = this.decodeJson(obj.getClass(), jsonResult.getString(i));
-                if(obj instanceof Force) {
-                    this.forces.add((Force) resultObject);
-                } else if (obj instanceof Crime) {
-                    this.crimes.add((Crime) resultObject);
+    // inner class for dealing with the getting and deserialising of crime and force objects from API
+    public class RequestTask extends AsyncTask<String, Void, String> {
+        private Object returnObjectType;
+
+        /**
+         * Creates a new instance of the RequestTask object.
+         * @param obj the type of object we expect to get back as represented by JSON
+         */
+        public RequestTask(Object obj) {
+            this.returnObjectType = obj;
+        }
+
+        public RequestTask() {
+            super();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            // params comes from the execute() call: params[0] is the url.
+            String result = "";
+            try {
+                result = this.downloadUrl(urls[0]);
+            } catch (IOException e) {
+                Log.d("OH DEAR", "Something went wrong");
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        private String downloadUrl(String myurl) throws IOException {
+            InputStream is = null;
+
+            try {
+                URL url = new URL(myurl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                // Starts the query
+                conn.connect();
+                int response = conn.getResponseCode();
+                Log.d("DEBUG", "The response is: " + response);
+                is = conn.getInputStream();
+
+                // Convert the InputStream into a string
+                String contentAsString = this.readIt(is);
+                return contentAsString;
+
+                // Makes sure that the InputStream is closed after the app is
+                // finished using it
+            } finally {
+                if (is != null) {
+                    is.close();
                 }
             }
-        } catch (JSONException e) {
-            e.printStackTrace();
         }
-        return;
-    }
 
-    // called on button press
-    public void getCrime(View view) {
+        // read HTTP response and return as string
+        private String readIt(InputStream stream) throws IOException, UnsupportedEncodingException {
+            BufferedReader in = new BufferedReader(new InputStreamReader(stream));
+            String line = null;
 
-    }
+            StringBuilder responseData = new StringBuilder();
+            while((line = in.readLine()) != null) {
+                responseData.append(line);
+            }
+            in.close();
+            return responseData.toString();
+        }
 
-    // turns JSON into clazz object
-    private <T> T decodeJson(Class<T> clazz, String json) {
-        Object obj = new Gson().fromJson(json, clazz);
-        return (T) obj;
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d("RESULTS", result);
+            try {
+                // deserialise the json objects and add to the appropriate instance variable
+                JSONArray jsonResult = new JSONArray(result);
+                for(int i = 0; i < jsonResult.length(); i++) {
+                    // for all results in array, deserialise into returnObjectType objects
+                    Object resultObject = this.decodeJson(this.returnObjectType.getClass(), jsonResult.getString(i));
+                    // and add to appropriate instance variables depending on class
+                    if(this.returnObjectType instanceof Force) {
+                        forces.add((Force) resultObject);
+                    } else if (this.returnObjectType instanceof Crime) {
+                        crimes.add((Crime) resultObject);
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            // populate spinners with new ly deserialised objects
+            populateSpinners();
+        }
+
+        // turns JSON into clazz object
+        private <T> T decodeJson(Class<T> clazz, String json) {
+            Object obj = new Gson().fromJson(json, clazz);
+            return (T) obj;
+        }
     }
 }
--- app/src/main/java/com/alacityfoundation/statistick/OnTaskCompleted.java
+++
@@ -1,8 +0,0 @@
-package com.alacityfoundation.statistick;
-
-/**
- * Created by ryan on 08/05/2015.
- */
-public interface OnTaskCompleted{
-    void onTaskCompleted(Object obj, String result);
-}
